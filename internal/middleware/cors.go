c877f476760e6a701d8a43a9fb8ac00278afa7fa
hung-senbox <mr.hung@senbox.org>
2025-11-08 16:04:22 +0700

    fix: cors midd

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -5,6 +5,7 @@ import "github.com/gin-gonic/gin"
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		origin := c.Request.Header.Get("Origin")
+		c.Writer.Header().Set("Vary", "Origin")
 		if origin != "" {
 			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 		} else {
@@ -13,9 +14,20 @@ func CORS() gin.HandlerFunc {
 
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Disposition")
-		c.Writer.Header().Set("Access-Control-Allow-Headers",
-			"Content-Type, Content-Length, Accept-Encoding, Accept-Language, X-CSRF-Token, Authorization, Cache-Control, X-Requested-With, X-App-Language")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		reqHeaders := c.Request.Header.Get("Access-Control-Request-Headers")
+		if reqHeaders != "" {
+			c.Writer.Header().Set("Access-Control-Allow-Headers", reqHeaders)
+		} else {
+			c.Writer.Header().Set("Access-Control-Allow-Headers",
+				"Content-Type, Content-Length, Accept, Accept-Encoding, Accept-Language, X-CSRF-Token, Authorization, Cache-Control, X-Requested-With, X-App-Language, ngrok-skip-browser-warning")
+		}
+		reqMethod := c.Request.Header.Get("Access-Control-Request-Method")
+		if reqMethod != "" {
+			c.Writer.Header().Set("Access-Control-Allow-Methods", reqMethod)
+		} else {
+			c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		}
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
